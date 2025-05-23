7fd23e18e4bc102a1ef7fc2f48c0a1a6e6e68a74
Fanxu Meng <60565778+fxmeng@users.noreply.github.com>
2025-05-23 23:36:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.remove_rope import RemoveRope
 from src.lora_qkv import LoraQKV
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model-path", type=str, default="meta-llama/Llama-3.2-1B/", help="Model to load")
+parser.add_argument("--model-path", type=str, default="meta-llama/Llama-2-7b-hf", help="Model to load")
 parser.add_argument("--save-path", type=str, default="outputs", help="output path.")
 parser.add_argument("--dtype", type=str, help="Data type to use.", choices=["fp32", "fp16", "bf16"], default="bf16")
 parser.add_argument("--device", type=str, help="Device to use.", choices=["cpu", "cuda", "auto"], default="auto")
@@ -18,12 +18,13 @@ parser.add_argument("--cal-max-seqlen", type=int, default=256, help="Maximum seq
 parser.add_argument("--varied-seqlen", action="store_true", help="Varied sequence lengths in the calibration data.")
 parser.add_argument("--seed", type=int, default=42, help="Seed for sampling the calibration data.")
 parser.add_argument("--ppl-eval-batch-size", type=int, default=8, help="Batch size for evaluating the perplexity.")
-parser.add_argument("--dim2head", type=int, default=4, help="")
+parser.add_argument("--dim2head", type=int, default=8, help="")
 parser.add_argument("--rope-head", type=int, default=1, help="")
 parser.add_argument("--qk-mqa-dim", type=int, default=64, help="")
+parser.add_argument("--collapse", type=int, default=2, help="")
 parser.add_argument("--q-lora-rank", type=int, help="")
-parser.add_argument("--kv-lora-rank", type=int, default=960, help="")
-parser.add_argument("--balance-kv-ratio", type=float, help="")
+parser.add_argument("--kv-lora-rank", type=int, default=512, help="")
+parser.add_argument("--balance-kv-ratio", type=float, default=1, help="")
 parser.add_argument("--use-qkv-norm", action='store_true', default=False, help="")
 args = parser.parse_args()
 
@@ -60,7 +61,7 @@ def main(args: argparse.Namespace) -> None:
         test_loader = prepare_test_dataloader(
             dataset=dataset["test"], tokenizer=tokenizer, batch_size=args.ppl_eval_batch_size
         )
-        #dataset_ppl = evaluate_ppl(model, tokenizer.pad_token_id, test_loader)
+        dataset_ppl = evaluate_ppl(model, tokenizer.pad_token_id, test_loader)
         print(f'Original ppl: {dataset_ppl:.4f}')
 
     print("+"*10+"RemoveRope Model:"+"+"*10)
@@ -70,11 +71,12 @@ def main(args: argparse.Namespace) -> None:
             ori_qkv_outputs["key"][layer_idx], 
             dim2head=args.dim2head, 
             rope_head=args.rope_head,
+            collapse=args.collapse,
         ))
         
     rm_rope_qkv_outputs = get_qkv_calibrate_outputs(model, train_loader)
     if args.ppl_eval_batch_size > 0:
-        #dataset_ppl = evaluate_ppl(model, tokenizer.pad_token_id, test_loader)
+        dataset_ppl = evaluate_ppl(model, tokenizer.pad_token_id, test_loader)
         print(f'Remove RoPE ppl: {dataset_ppl:.4f}')
 
     print("+"*10+"LoraQKV Model:"+"+"*10)
@@ -87,6 +89,7 @@ def main(args: argparse.Namespace) -> None:
             rm_rope_qkv_outputs["value"][layer_idx], 
             q_lora_rank=args.q_lora_rank, 
             qk_mqa_dim=args.qk_mqa_dim, 
+            collapse=args.collapse,
             kv_lora_rank=args.kv_lora_rank,
             use_qkv_norm=args.use_qkv_norm,
             balance_kv_ratio=args.balance_kv_ratio,
