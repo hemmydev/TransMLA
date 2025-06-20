eab7529022276e099e1efc82c908a863bf69c78b
fxmeng <filtergrafting@gmail.com>
2025-06-20 15:49:15 +0800

    pretrain

---
+++ pretrain/run_llama2_7b.sh
@@ -0,0 +1,25 @@
+BASE_MODEL="outputs/llama2-7B-deepseek"
+OUTPUT_PATH="outputs/ft6b-outputs/llama2-7B-deepseek"
+DATA_PATH="fxmeng/transmla_pretrain_6B_tokens"
+
+export HF_ENDPOINT=https://hf-mirror.com
+# batch size = per_device_train_batch_size * gradient_accumulation_steps * num_gpus = 128
+deepspeed --master_port=16971 --include=localhost:0,1,2,3,4,5,6,7 train.py \
+    --deepspeed configs/ds_config_zero3.json \
+    --model_name_or_path $BASE_MODEL \
+    --bf16 \
+    --data_path $DATA_PATH \
+    --output_dir $OUTPUT_PATH \
+    --num_train_epochs 1 \
+    --seq_len 4096 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 2 \
+    --save_strategy "steps" \
+    --save_steps 100 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0.01 \
+    --warmup_ratio 0.03 \
+    --logging_steps 1 \
+    --lr_scheduler_type "cosine" \
+    --report_to "tensorboard"
